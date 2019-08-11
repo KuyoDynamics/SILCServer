2b114e1486e63e8a8297ead62dc3ef9d3178f121
Chaiwa Berian <chaiwa.berian@gmail.com>
2019-08-11 21:45:53 +0200

    Cleanups on User Role and Permissions Routers

--- server/app/silc_api/routes/user/user_role_router.js
+++ server/app/silc_api/routes/user/user_role_router.js
@@ -1,12 +1,11 @@
 let router = require('express').Router();
-let controller = require('../controllers/user_controller');
+let controller = require('../controllers/user_role_controller');
 let verify_auth_scope = require('../../../helpers/authentication/jwt_auth_scope_verifier');
 let {require_authentication} = require('../../../helpers/authentication/authentication_manager');
 
 router.route('/:id')
-    .get(require_authentication,verify_auth_scope(['read:users']), controller.getUser)
+    .get(require_authentication,verify_auth_scope(['read:user_roles']), controller.getUserRole)
 router.route('/')
-    .post(controller.createUser) //User create should not be authenticated
-//All other routes below should require authentication
+    .post(require_authentication,verify_auth_scope(['create:user_roles']),controller.createUserRole)
 
 module.exports = router;
